69f6d5678be88f2fef7cb5813ebaf9c20aa64454
antizwiebel <Penedermark@gmail.com>
2016-03-09 12:04:52 +0100

    Committing the Java-File

--- felpower_antizwiebel_java/DummyProject/src/MainActivity.java
+++ felpower_antizwiebel_java/DummyProject/src/MainActivity.java
@@ -4,6 +4,7 @@ public class MainActivity {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Wolrd");
+		System.out.println("HalliHallo");
 	}
 
 }
